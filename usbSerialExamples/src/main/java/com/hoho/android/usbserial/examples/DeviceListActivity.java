31caecb0b189aa21bf6d0ccbb3568af6710ee9bd
nuwanprabhath <nuwanprabhath@gmail.com>
2015-09-03 22:51:00 +0530

    After push

--- usbSerialExamples/src/main/java/com/hoho/android/usbserial/examples/DeviceListActivity.java
+++ usbSerialExamples/src/main/java/com/hoho/android/usbserial/examples/DeviceListActivity.java
@@ -1,460 +1,53 @@
-/* Copyright 2011-2013 Google Inc.
- * Copyright 2013 mike wakerly <opensource@hoho.com>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
- * USA.
- *
- * Project home page: https://github.com/mik3y/usb-serial-for-android
- */
-
 package com.hoho.android.usbserial.examples;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.hardware.usb.UsbDevice;
-import android.hardware.usb.UsbDeviceConnection;
-import android.hardware.usb.UsbManager;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
-import android.os.SystemClock;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.TwoLineListItem;
-
-import com.couchbase.lite.Database;
-import com.couchbase.lite.Document;
-import com.couchbase.lite.Emitter;
-import com.couchbase.lite.Manager;
-import com.couchbase.lite.Mapper;
-import com.couchbase.lite.android.AndroidContext;
-import com.couchbase.lite.replicator.Replication;
-import com.hoho.android.usbserial.driver.UsbSerialDriver;
-import com.hoho.android.usbserial.driver.UsbSerialPort;
-import com.hoho.android.usbserial.driver.UsbSerialProber;
-import com.hoho.android.usbserial.util.HexDump;
-import com.hoho.android.usbserial.util.SerialInputOutputManager;
-
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
- * Shows a {@link ListView} of available USB devices.
- *
- * @author mike wakerly (opensource@hoho.com)
+ * Main activity in application which start background process to handle USB data receive and sync.
  */
-public class DeviceListActivity extends Activity implements Replication.ChangeListener, UsbDataReceiver.Receiver {
+public class DeviceListActivity extends Activity implements UsbDataReceiver.Receiver {
 
     private final String TAG = DeviceListActivity.class.getSimpleName();
 
-    private static UsbSerialPort sPort = null;
-
-    Thread t;
-    AsyncTask as;
-
-
-    private volatile Boolean started = false;
-
-    private UsbManager mUsbManager;
-    //    private ListView mListView;
     private TextView mProgressBarTitle;
     private TextView mDumpTextView;
     private TextView demoTitle;
     private TextView mCOValue;
     private ProgressBar mProgressBar;
     private ScrollView mScrollView;
-    private List<UsbSerialPort> mEntries = new ArrayList<>();
-
-    private final ExecutorService mExecutor = Executors.newSingleThreadExecutor();
-
-    private final ExecutorService threadpool = Executors.newFixedThreadPool(5);
-
-    private SerialInputOutputManager mSerialIoManager;
     private UsbDataReceiver mReceiver;
-    private final SerialInputOutputManager.Listener mListener =
-            new SerialInputOutputManager.Listener() {
-
-                @Override
-                public void onRunError(Exception e) {
-                    Log.d(TAG, "Runner stopped.");
-
-//                    Toast.makeText(getApplicationContext(),"Runner stopped." ,Toast.LENGTH_LONG);
-                    mDumpTextView.append("Runner stopped.");
-                }
-
-                @Override
-                public void onNewData(final byte[] data) {
-                    Runnable a = new Runnable() {
-                        @Override
-                        public void run() {
-
-//                            Toast.makeText(getApplicationContext(),"on new data." ,Toast.LENGTH_LONG);
-//                            mDumpTextView.append("on new data.");
-
-                            DeviceListActivity.this.updateReceivedData(data);
-                        }
-                    };
-
-                    threadpool.execute(a);
-                }
-            };
-
-
-    /////////////
-    ////couchbase vari
-
-//    public static String TAG = "GrocerySync";
-
-    //constants
-    public static final String DATABASE_NAME = "grocery-sync";
-    public static final String designDocName = "grocery-local";
-    public static final String byDateViewName = "byDate";
-
-
-    public static final String SYNC_URL = "http://192.248.8.247:4984/sync_gateway";
-
-    //couch internals
-    protected static Manager manager;
-    private Database database;
-    //////////////
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
-//        mListView = (ListView) findViewById(R.id.deviceList);
-        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
         mProgressBarTitle = (TextView) findViewById(R.id.progressBarTitle);
-
         demoTitle = (TextView) findViewById(R.id.demoTitle);
-
         mDumpTextView = (TextView) findViewById(R.id.consoleText);
-        mScrollView = (ScrollView) findViewById(R.id.demoScroller);
         mCOValue = (TextView) findViewById(R.id.co_value);
+
+        mScrollView = (ScrollView) findViewById(R.id.demoScroller);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
 
         mReceiver = new UsbDataReceiver(new Handler());
         mReceiver.setReceiver(this);
         Intent intent = new Intent(Intent.ACTION_SYNC, null, this, UsbSerialService.class);
         intent.putExtra("receiver", mReceiver);
         startService(intent);
-//KEEP
-//
-//        t = new Thread(){
-//            @Override
-//            public void run() {
-//                while(!started) {
-//                    Log.d(TAG, started + "");
-//                    boolean a = refreshDeviceList();
-//                    Log.d(TAG,a+"");
-//                    try {
-//                        Thread.sleep(1000);
-//                    } catch (InterruptedException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            }
-//        };
-//
-//        t.start();
-
-//        try {
-//            startCBLite();
-//        } catch (Exception e) {
-//            Toast.makeText(getApplicationContext(), "Error Initializing CBLIte, see logs for details", Toast.LENGTH_LONG).show();
-//            com.couchbase.lite.util.Log.e(TAG, "Error initializing CBLite", e);
-//        }
-    }
-
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        mHandler.sendEmptyMessage(MESSAGE_REFRESH);
-//    }
-//
-//    @Override
-//    protected void onPause() {
-//        super.onPause();
-//        mHandler.removeMessages(MESSAGE_REFRESH);
-//    }
-
-    private boolean refreshDeviceList() {
-
-        Log.d(TAG, "AAAAAAAAAAA");
-
-//        showProgressBar();
-
-//        as = new AsyncTask<Void, Void, List<UsbSerialPort>>() {
-//            @Override
-//            protected List<UsbSerialPort> doInBackground(Void... params) {
-        Log.d(TAG, "Refreshing device list ...");
-//                SystemClock.sleep(1000);
-
-        final List<UsbSerialDriver> drivers =
-                UsbSerialProber.getDefaultProber().findAllDrivers(mUsbManager);
-
-        final List<UsbSerialPort> result = new ArrayList<>();
-        for (final UsbSerialDriver driver : drivers) {
-            final List<UsbSerialPort> ports = driver.getPorts();
-            Log.d(TAG, String.format("+ %s: %s port%s",
-                    driver, Integer.valueOf(ports.size()), ports.size() == 1 ? "" : "s"));
-            result.addAll(ports);
-        }
-
-//                return result;
-//            }
-//
-//            @Override
-//            protected void onPostExecute(List<UsbSerialPort> result) {
-        if (!started) {
-            mEntries.clear();
-            mEntries.addAll(result);
-//                mAdapter.notifyDataSetChanged();
-
-//                hideProgressBar();
-//                Log.d(TAG, "Done refreshing, " + mEntries.size() + " entries found.");
-
-//                mDumpTextView.append("aaaaaaaaa");
-//                mDumpTextView.append("aaaaaaaaa");
-//                mDumpTextView.append("aaaaaaaaa");
-
-//                    mProgressBarTitle.setText(
-//                            String.format("%s device(s) found", Integer.valueOf(mEntries.size())));
-
-            if (mEntries.size() > 0) {
-                sPort = mEntries.get(0);
-                started = true;
-                startConsole();
-                return true;
-            }
-
-//                while(mEntries.size()>0);
-        }
-//                hideProgressBar();
-        Log.d(TAG, "hide progress bar");
-//            }
-//
-//        }.execute((Void) null);
-        return false;
-
-    }
-
-    private void showProgressBar() {
-        mProgressBar.setVisibility(View.VISIBLE);
-        mProgressBarTitle.setText(R.string.refreshing);
     }
 
     private void hideProgressBar() {
         mProgressBar.setVisibility(View.INVISIBLE);
-    }
-
-    private void showConsoleActivity(UsbSerialPort port) {
-        SerialConsoleActivity.show(this, port);
-    }
-
-
-    protected void startConsole() {
-        if (sPort == null) {
-
-        } else {
-            final UsbManager usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
-
-            UsbDeviceConnection connection = usbManager.openDevice(sPort.getDriver().getDevice());
-            if (connection == null) {
-
-                return;
-            }
-
-            try {
-                sPort.open(connection);
-                sPort.setParameters(115200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
-            } catch (IOException e) {
-
-                Log.e(TAG, "Error setting up device: " + e.getMessage(), e);
-                try {
-                    sPort.close();
-                } catch (IOException e2) {
-                    // Ignore.
-                }
-                sPort = null;
-                return;
-            }
-        }
-        onDeviceStateChange();
-    }
-
-    private void stopIoManager() {
-        if (mSerialIoManager != null) {
-            Log.i(TAG, "Stopping io manager ..");
-            mSerialIoManager.stop();
-            mSerialIoManager = null;
-        }
-    }
-
-    private void startIoManager() {
-        if (sPort != null) {
-            Log.i(TAG, "Starting io manager ..");
-            mSerialIoManager = new SerialInputOutputManager(sPort, mListener);
-            mExecutor.submit(mSerialIoManager);
-        }
-    }
-
-    private void onDeviceStateChange() {
-        stopIoManager();
-        startIoManager();
-    }
-
-    private void updateReceivedData(byte[] data) {
-        String a = new String(data);
-        final String message = "Read " + a + "  ======" + "\n\n" + started;
-//        mDumpTextView.append(message);
-//        mScrollView.smoothScrollTo(0, mDumpTextView.getBottom());
-
-        try {
-            createGroceryItem(message);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-//        Toast.makeText(getApplicationContext(),message ,Toast.LENGTH_LONG);
-
-        Log.d(TAG, started + "");
-    }
-
-///////////////////////////////////////////////////////////////////
-    ////couchbase
-
-    protected void startCBLite() throws Exception {
-
-        Manager.enableLogging(TAG, com.couchbase.lite.util.Log.VERBOSE);
-        Manager.enableLogging(com.couchbase.lite.util.Log.TAG, com.couchbase.lite.util.Log.VERBOSE);
-        Manager.enableLogging(com.couchbase.lite.util.Log.TAG_SYNC_ASYNC_TASK, com.couchbase.lite.util.Log.VERBOSE);
-        Manager.enableLogging(com.couchbase.lite.util.Log.TAG_SYNC, com.couchbase.lite.util.Log.VERBOSE);
-        Manager.enableLogging(com.couchbase.lite.util.Log.TAG_QUERY, com.couchbase.lite.util.Log.VERBOSE);
-        Manager.enableLogging(com.couchbase.lite.util.Log.TAG_VIEW, com.couchbase.lite.util.Log.VERBOSE);
-        Manager.enableLogging(com.couchbase.lite.util.Log.TAG_DATABASE, com.couchbase.lite.util.Log.VERBOSE);
-
-        manager = new Manager(new AndroidContext(this), Manager.DEFAULT_OPTIONS);
-
-        //install a view definition needed by the application
-        database = manager.getDatabase(DATABASE_NAME);
-        com.couchbase.lite.View viewItemsByDate = database.getView(String.format("%s/%s", designDocName, byDateViewName));
-        viewItemsByDate.setMap(new Mapper() {
-            @Override
-            public void map(Map<String, Object> document, Emitter emitter) {
-                Object createdAt = document.get("created_at");
-                if (createdAt != null) {
-                    emitter.emit(createdAt.toString(), null);
-                }
-            }
-        }, "1.0");
-
-        startSync();
-    }
-
-    private void startSync() {
-
-        URL syncUrl;
-        try {
-            syncUrl = new URL(SYNC_URL);
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
-
-        Replication pullReplication = database.createPullReplication(syncUrl);
-        pullReplication.setContinuous(true);
-
-        Replication pushReplication = database.createPushReplication(syncUrl);
-        pushReplication.setContinuous(true);
-
-        pullReplication.start();
-        pushReplication.start();
-
-        pullReplication.addChangeListener(this);
-        pushReplication.addChangeListener(this);
-
-    }
-
-    private Document createGroceryItem(String text) throws Exception {
-
-        SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-
-        UUID uuid = UUID.randomUUID();
-        Calendar calendar = GregorianCalendar.getInstance();
-        long currentTime = calendar.getTimeInMillis();
-        String currentTimeString = dateFormatter.format(calendar.getTime());
-
-        String id = currentTime + "-" + uuid.toString();
-
-        Document document = database.createDocument();
-
-        Map<String, Object> properties = new HashMap<>();
-        properties.put("_id", id);
-        properties.put("text", text);
-        properties.put("check", Boolean.FALSE);
-        properties.put("created_at", currentTimeString);
-        document.putProperties(properties);
-
-        com.couchbase.lite.util.Log.d(TAG, "Created new grocery item with id: %s", document.getId());
-
-        return document;
-    }
-
-    @Override
-    public void changed(Replication.ChangeEvent event) {
-
-        Replication replication = event.getSource();
-        com.couchbase.lite.util.Log.d(TAG, "Replication : " + replication + " changed.");
-        if (!replication.isRunning()) {
-            String msg = String.format("Replicator %s not running", replication);
-            com.couchbase.lite.util.Log.d(TAG, msg);
-        } else {
-            int processed = replication.getCompletedChangesCount();
-            int total = replication.getChangesCount();
-            String msg = String.format("Replicator processed %d / %d", processed, total);
-            com.couchbase.lite.util.Log.d(TAG, msg);
-        }
-
-        if (event.getError() != null) {
-            //showError("Sync error", event.getError());
-        }
-
     }
 
     public void showError(final String errorMessage) {
@@ -469,14 +62,18 @@ public class DeviceListActivity extends Activity implements Replication.ChangeLi
 
     }
 
+    /**
+     * Receive data from background process
+     * @param resultCode
+     * @param resultData
+     */
     @Override
     public void onReceiveResult(int resultCode, Bundle resultData) {
         Log.d(TAG, "Service Result on DeviceListActivity");
         switch (resultCode) {
 
             case UsbSerialService.STATUS_RUNNING:
-                Log.d(TAG, "DATABASE_NAME");
-                //setProgressBarIndeterminateVisibility(true);
+                Log.d(TAG, "SERVICE_RUNNING");
                 break;
 
             case UsbSerialService.STATUS_DEVICE_FOUND:
@@ -494,24 +91,17 @@ public class DeviceListActivity extends Activity implements Replication.ChangeLi
                 Toast.makeText(this, error, Toast.LENGTH_LONG).show();
                 Log.d(TAG, "USB_STATUS_ERROR");
                 break;
+
             case UsbSerialService.STATUS_ERROR_COUCHBASE:
                 showError("Error Connecting to Server");
                 break;
+
+            case UsbSerialService.STATUS_GPS_OFF:
+                Utils.displayPromptForEnablingGPS(this);
+                break;
+
         }
     }
 
-    private String[] getGasValues(String data) {
-        String[] gasses = data.split(";");
-        String[] gasValues = new String[3];
-        int i = 0;
-        for (String gas : gasses) {
-            Log.d(TAG, "GAS_VALUE" + gas);
-            String[] gasData = gas.split(":");
-            if (gasData.length == 3) {
-                gasValues[i] = gasData[2];
 
-            }
-        }
-        return gasValues;
-    }
 }
---
+++ usbSerialExamples/src/main/java/com/hoho/android/usbserial/examples/UserEmailFetcher.java
@@ -0,0 +1,39 @@
+package com.hoho.android.usbserial.examples;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Context;
+
+/**
+ * Created by Nuwan Prabhath on 9/3/2015.
+ */
+public class UserEmailFetcher {
+
+    /**
+     * Get User default email address
+     * @param context
+     * @return
+     */
+    static String getEmail(Context context) {
+
+        AccountManager accountManager = AccountManager.get(context);
+        Account account = getAccount(accountManager);
+
+        if (account == null) {
+            return null;
+        } else {
+            return account.name;
+        }
+    }
+
+    private static Account getAccount(AccountManager accountManager) {
+        Account[] accounts = accountManager.getAccountsByType("com.google");
+        Account account;
+        if (accounts.length > 0) {
+            account = accounts[0];
+        } else {
+            account = null;
+        }
+        return account;
+    }
+}
